e4ca1d965cba8c2e4eafec2552a2d85df2811ae7
帝苑 <paul@pauldemacbook-pro.local>
2016-04-04 16:35:20 +0800

    Initial Commit

---
+++ RTU5023/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTU5023
+//
+//  Created by paul on 16/4/4.
+//  Copyright © 2016年 paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RTU5023/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTU5023
+//
+//  Created by paul on 16/4/4.
+//  Copyright © 2016年 paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

e429cfab5022b17e7470c7715b526c8358254f27
Coolzhou <colzhou@126.com>
2018-07-29 17:08:13 +0800

    test

--- RTU5023/SetServerController.m
+++ RTU5023/SetServerController.m
@@ -12,6 +12,8 @@
 @property (weak, nonatomic) IBOutlet UIView *bgView;
 @property (weak, nonatomic) IBOutlet UIScrollView *bgView2;
 
+@property (nonatomic, strong) NSArray *dataArr;
+
 @end
 
 @implementation SetServerController
@@ -24,6 +26,36 @@
     
     self.bgView.layer.borderColor = THEAPPDELEGATE.borderColor.CGColor;
     self.bgView2.layer.borderColor = THEAPPDELEGATE.borderColor.CGColor;
+    
+    self.dataArr = [NSArray arrayWithObjects:@"1、Modbus RTU协议",@"2、Modbus TCP协议",@"3、自定义协议", nil];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.dataArr.count;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    UILabel *myView = nil;
+    NSString *t = nil;
+    t = [self.dataArr objectAtIndex:row];
+    myView = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, self.pickView.frame.size.width, 20)];
+    myView.textAlignment = NSTextAlignmentCenter;
+    myView.text = t;
+    myView.font = [UIFont systemFontOfSize:13];         //用label来设置字体大小
+    myView.backgroundColor = [UIColor clearColor];
+    return myView;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    
+    return 50.0f;
+}
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+    return 20.0f;
 }
 
 - (IBAction)clickSetupBtn1:(id)sender {
@@ -44,7 +76,11 @@
 
 //picker ID
 - (IBAction)clickSetupBtn2:(id)sender {
-    NSString *mg = [NSString stringWithFormat:@"%@GPRSON%@", THEAPPDELEGATE.sel_host_pwd];
+    
+    NSInteger row = [self.pickView selectedRowInComponent:0];
+    NSLog(@"row = %ld",row);
+    
+    NSString *mg = [NSString stringWithFormat:@"%@GPRSON%ld", THEAPPDELEGATE.sel_host_pwd,row+1];
     [THEAPPDELEGATE.mainV sendMsg:mg phNum:THEAPPDELEGATE.sel_host_phNum];
 }
 
